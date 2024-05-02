f7b39951733a4245b1307703bc75d25582c01173
Arnthorny <chidubem.anthony26@gmail.com>
2024-05-02 14:34:57 -0700

    Added space optimization

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,20 +12,25 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
+    coins.sort(reverse=True)
+
     # This array stores the solutions for each sub-problem
-    dp_arr = [total + 100] * (total + 1)
-    dp_arr[0] = 0
+    dp_cache = {0: 0}
 
     # Loop through every sub-problem in dp_arr. From index = 0 to index = total
-    for i in range(len(dp_arr)):
-        for j in range(len(coins)):
+    for i in range(1, total + 1):
+        min_coins = total + 100
+        for coin in coins:
             # Check that current coin value is less than current sub-problem.
-            if coins[j] <= i:
-                # Set the soln of curent sub-problem to be the minimum of
-                # its current soln and 1 + the solution to a previously
-                # solved sub-problem
-                dp_arr[i] = min(dp_arr[i], 1 + dp_arr[i - coins[j]])
+            if coin <= i:
+                # If previous sub-problem solution exists in cache
+                if i - coin in dp_cache:
+                    # Compare with the current minimum
+                    min_coins = min(min_coins, 1 + dp_cache[i - coin])
+
+        # Set the soln of curent sub-problem to be the minimum
+        dp_cache[i] = min_coins
 
     # If value at index of sub-problem exceeds the sub-problem, then no valid
     # solution. Else the value at that index is the solution.
-    return -1 if dp_arr[total] > total else dp_arr[total]
+    return -1 if dp_cache[total] > total else dp_cache[total]
