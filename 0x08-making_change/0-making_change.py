3b06c97ebc12470d0a6510590cff7893affe87fe
Arnthorny <chidubem.anthony26@gmail.com>
2024-05-04 05:46:44 -0700

    Minor optimization added

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -15,21 +15,22 @@ def makeChange(coins, total):
     coins.sort(reverse=True)
 
     # This array stores the solutions for each sub-problem
-    dp_cache = {0: 0}
+    total_1 = total + 1
+    dp_cache = [total_1]  * (total_1)
+    dp_cache[0] = 0
 
     # Loop through every sub-problem in dp_arr. From index = 0 to index = total
-    for i in range(1, total + 1):
-        min_coins = total + 100
+    for i in range(1, total_1):
         for coin in coins:
             # Check that current coin value is less than current sub-problem.
             if coin <= i:
                 # If previous sub-problem solution exists in cache
-                if i - coin in dp_cache:
-                    # Compare with the current minimum
-                    min_coins = min(min_coins, 1 + dp_cache[i - coin])
+                tmp = dp_cache[i - coin] + 1
+                # Compare with the current minimum
+                dp_cache[i] = total_1 if total_1 < tmp else tmp
+                break
 
         # Set the soln of curent sub-problem to be the minimum
-        dp_cache[i] = min_coins
 
     # If value at index of sub-problem exceeds the sub-problem, then no valid
     # solution. Else the value at that index is the solution.
